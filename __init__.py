d41d70700d3b82dc799dc4b9b6e74f2df0ee1229
Steve R <steversig@virginmedia.com>
2020-12-18 18:25:43 +0000

    gets temperatures for house and individual rooms

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from mycroft.util.log import getLogger
 from wiserHeatingAPI import wiserHub
 import json
 import sys
+import os
 
 __author__ = 'steversig'
 LOGGER = getLogger(__name__)
@@ -18,20 +19,43 @@ class WiserHeatingSkill(MycroftSkill):
         super().__init__(name="WiserHeatingSkill")
         self.wh = None
 
-    def initialize(self):
-        my_setting = self.settings.get('my_setting')
-        wiserkey=""
-        wiserhubip=""
+    def _setup(self, force=False):
+        try:
+            pwd = os.path.dirname(os.path.abspath(__file__))
+            fpath = os.path.join(pwd, 'wiserkeys.params')
+            LOGGER.info(fpath)
+            with open(fpath, 'r') as f:
+                data = f.read().split('\n')
+        except FileNotFoundError as e:
+            LOGGER.debug("{}, {}".format(e.strerror, 'wiserkeys.params')    )
+        else:
+            wiserkey=""
+            wiserip=""
+
+            for lines in data:
+                line=lines.split('=')
+                if line[0]=='wiserkey':
+                    wiserkey=line[1]
+                if line[0]=='wiserhubip':
+                    wiserhubip=line[1]
+
+        if self.settings.get('wiserkey') is not None:
+            wiserkey = self.settings.get('wiserkey')
+        if self.settings.get('wiserhubip') is not None:
+            wiserhubip = self.settings.get('wiserhubip')
         # connect to the wiser hub
         try:
             try:
                 self.wh = wiserHub.wiserHub(wiserhubip,wiserkey)
             except:
                 LOGGER.debug("Unable to connect to Wiser Hub {}".format(sys.exc_info()[1])    )
-                LOGGER.debug (' Wiser Hub IP= {}'.format(wiserip))
+                LOGGER.debug (' Wiser Hub IP= {}'.format(wiserhubip))
                 LOGGER.debug (' WiserKey= {}'.format(wiserkey))
         except json.decoder.JSONDecodeError as ex:
             LOGGER.debug("JSON Exception")
+
+    def initialize(self):
+        self._setup()
 
     @intent_file_handler('heating.wiser.advance.intent')
     def handle_heating_wiser_advance(self, message):
@@ -47,14 +71,20 @@ class WiserHeatingSkill(MycroftSkill):
 
     @intent_file_handler('heating.wiser.getroomtemp.intent')
     def handle_heating_wiser_getroomtemp(self, message):
-        adv_room ="All"
+        #self._setup()
+        myroom = message.data["wiserroom"].lower()
         response = ""
         for room in self.wh.getRooms():
-            name = room.get("Name")
-            if adv_room == "All" or name == adv_room:
+            name = room.get("Name").lower()
+            if myroom == "house" or name == myroom:
                 response += "{} {} degrees ".format(name,room.get("CalculatedTemperature")/10)
-        LOGGER.info(response)
-        self.speak_dialog(response.replace('.', ' point '))
+        LOGGER.info("wiser getroomtemp: {}".format(response))
+        if response != "":
+            self.speak_dialog(response.replace('.', ' point '))
+        else:
+            self.speak_dialog('heating.wiser.unknown.room', data={
+                              "wiserroom": myroom})
+#            self.speak_dialog("unknown")
 
     @intent_file_handler('heating.wiser.setroomtemp.intent')
     def handle_heating_wiser_setroomtemp(self, message):
